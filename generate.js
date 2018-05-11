ebc2173ba706f6516c686ea3d6af3b60bf62f317
pkrv13 <37273200+pkrv13@users.noreply.github.com>
2018-05-11 13:38:01 +0700

    initial commit
    symbol match functional

---
+++ generate.js
@@ -0,0 +1,162 @@
+/**
+ * @author Piotr
+ */
+
+var tileImages;
+var tileBg;
+
+var flippedSymbolId;
+var flippedTileImg;
+
+var boardState;
+
+//statistics
+var flipsSoFar;
+var startTime;
+
+//score
+var currentScore;
+var scoreToWin;
+
+function loadImages(inPath, inCount)
+{
+	tileImages = new Array(inCount);
+	for(i = 0; i < inCount; ++i)
+	{
+		tileImages[i] = new Image();
+		tileImages[i].src = inPath + 'tile (' + i + ').png';
+	}
+	
+	tileBg = new Image();
+	tileBg.src = inPath +  'tile-back.png';
+	
+	var bgPath = inPath + 'bg.jpg';
+}
+
+function startNewGame()
+{
+	//see what size is needed
+	var dropdownX = document.getElementById('DropDownX');
+	var width = dropdownX.options[ dropdownX.selectedIndex ].value;
+	var dropdownY = document.getElementById('DropDownY'); 
+	var height = dropdownY.options[ dropdownY.selectedIndex ].value;
+
+	//drop previous game content and reset state
+	var table = document.getElementById('PlayAreaTable');
+	table.innerHTML = "";
+	currentScore = 0;
+	scoreToWin = width * height / 2;
+	
+	var cellId = 0;
+	for(y = 0; y < height; ++y)
+	{
+		var addedRow = table.insertRow(y);
+		for(x = 0; x < width; ++x)
+		{
+			var addedCell = addedRow.insertCell(x);
+
+			var newImg = document.createElement('img');
+			newImg.id = "" + cellId;
+			newImg.src = tileBg.src;
+			newImg.addEventListener('click', function(){
+				imageClicked(this);
+			});
+			newImg.className = "Tile";
+			addedCell.appendChild(newImg);
+
+			
+			++cellId;
+		}
+	}
+	
+	createBoardState(width * height);
+
+	startTime = new Date();
+	
+	//don't use the same images all over just because they are earlier on the list
+	shuffle(tileImages);
+}
+
+function createBoardState(inTileCount)
+{
+	boardState = new Array(inTileCount);
+	for(i = 0; i < inTileCount / 2; ++i)
+	{
+		boardState[i * 2] = i;
+		boardState[i * 2 + 1] = i;
+	}
+	shuffle(boardState);
+	flippedSymbolId = -1;
+}
+
+function hideFlippedTiles(tile1, tile2)
+{
+	flippedSymbolId = -1;
+	tile1.src = tileBg.src;	
+	tile2.src = tileBg.src;	
+}
+
+function scoreFlippedTiles(tile1, tile2)
+{
+	tile1.id = null;
+	tile2.id = null;
+	flippedSymbolId = -1;
+	currentScore++;
+	var scoreText = document.getElementById('Score');
+	scoreText.innerHTML = 'Score: '+ currentScore;	
+	if(currentScore === scoreToWin)
+	{
+		var endTime = new Date();
+		var duration = endTime - startTime; 
+	}
+}
+
+function imageClicked(img)
+{
+	if(flippedSymbolId === null || img.id === null || img === flippedTileImg)
+	{
+		return;
+	}
+	
+	var newlyFlippedSymbolId = boardState[img.id];
+	img.src = tileImages[boardState[img.id]].src;
+
+	if(flippedSymbolId === -1)
+	{
+		flippedSymbolId = newlyFlippedSymbolId;
+		flippedTileImg = img;
+	}
+	else
+	{
+		if(flippedSymbolId !== newlyFlippedSymbolId)
+		{
+			setTimeout(hideFlippedTiles.bind(null, img, flippedTileImg), 1200);
+			flippedSymbolId = null;
+		}
+		else
+		{
+			scoreFlippedTiles(img, flippedTileImg);
+		}
+	}
+}
+
+//utility func from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array)
+{
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
